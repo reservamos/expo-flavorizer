c69158bd378a965c624e00f3211fc01385550460
ErnestoVM <arkangel_121@hotmail.com>
2024-06-07 07:32:11 -0600

    feat: add test with malformed build gradle files with custom config

--- src/processors/android/buildGradleProcessorCustomConfig.test.js
+++ src/processors/android/buildGradleProcessorCustomConfig.test.js
@@ -21,3 +21,66 @@ describe("AndroidBuildGradleProcessor", () => {
     expect(stripEndOfLines(result)).toEqual(stripEndOfLines(matcher));
   });
 });
+
+describe("AndroidBuildGradleProcessor", () => {
+  const content = fs.readFileSync(
+    `${process.cwd()}/testResources/android/buildGradleProcessorTest/buildIdempotent.gradle`,
+    "utf8"
+  );
+  const matcher = fs.readFileSync(
+    `${process.cwd()}/testResources/android/buildGradleProcessorCustomConfigTest/buildExpected.gradle`,
+    "utf8"
+  );
+
+  it("Test idempotent AndroidBuildGradleProcessor with custom config", () => {
+    const result = AndroidBuildGradleProcessor(content, config);
+    expect(stripEndOfLines(result)).toEqual(stripEndOfLines(matcher));
+  });
+});
+
+describe("AndroidBuildGradleProcessor", () => {
+  it("Test malformed AndroidBuildGradleProcessor with custom config", () => {
+    expect(() => {
+      AndroidBuildGradleProcessor("", config);
+    }).toThrow();
+  });
+});
+
+describe("AndroidBuildGradleProcessor", () => {
+  const content = fs.readFileSync(
+    `${process.cwd()}/testResources/android/buildGradleProcessorTest/buildMalformedOne.gradle`,
+    "utf8"
+  );
+
+  it("Test existing flavor dimensions exception AndroidBuildGradleProcessor with custom config", () => {
+    expect(() => {
+      AndroidBuildGradleProcessor(content, config);
+    }).toThrow();
+  });
+});
+
+describe("AndroidBuildGradleProcessor", () => {
+  const content = fs.readFileSync(
+    `${process.cwd()}/testResources/android/buildGradleProcessorTest/buildMalformedTwo.gradle`,
+    "utf8"
+  );
+
+  it("Test malformed AndroidBuildGradleProcessor with custom config", () => {
+    expect(() => {
+      AndroidBuildGradleProcessor(content, config);
+    }).toThrow();
+  });
+});
+
+describe("AndroidBuildGradleProcessor", () => {
+  const content = fs.readFileSync(
+    `${process.cwd()}/testResources/android/buildGradleProcessorTest/buildMalformedThree.gradle`,
+    "utf8"
+  );
+
+  it("Test malformed AndroidBuildGradleProcessor with custom config", () => {
+    expect(() => {
+      AndroidBuildGradleProcessor(content, config);
+    }).toThrow();
+  });
+});
