f3ca603440d34405ec49070c3d2c44728273bb5e
ErnestoVM <arkangel_121@hotmail.com>
2024-08-17 15:58:03 -0600

    Merge branch 'ft/processor-ios-plist'

---
+++ src/processors/ios/plistProcessor.test.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const configLoader = require("../../utils/configLoader");
+const IosPlistProcessor = require("./plistProcessor");
+const constants = require("../../utils/constants");
+const configFilePath = `${process.cwd()}/testResources/${
+  constants.CONFIG_FILE
+}`;
+const config = configLoader(configFilePath);
+const stripEndOfLines = require("../../utils/testUtils");
+
+describe("IosPlistProcessor", () => {
+  const plistPath = `${process.cwd()}/testResources/ios/plistProcessorTest/Info.plist`;
+
+  const matcher = fs.readFileSync(
+    `${process.cwd()}/testResources/ios/plistProcessorTest/Matcher.plist`,
+    "utf8"
+  );
+
+  it("Test IosPlistProcessor", async () => {
+    const result = await IosPlistProcessor(plistPath, config);
+    expect(stripEndOfLines(result)).toEqual(stripEndOfLines(matcher));
+  });
+});
